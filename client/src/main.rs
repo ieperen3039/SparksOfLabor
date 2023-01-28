fe75cbc7aa292c094705048e33f3a34e5dde88f1
Geert van Ieperen <ieperen3039@outlook.com>
2023-01-28 17:14:11 +0100

    Added a bit more infrastructure

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,10 +1,24 @@
 #![allow(dead_code)]
+// #extern crate glfw;
+// pub mod vector_alias;
+// pub mod voxel_engine;
 
-extern crate glfw;
-
-pub mod vector_alias;
-pub mod voxel_engine;
+extern crate zmq;
 
 fn main() {
+    println!("Player: online");
+    let ctx = zmq::Context::new();
 
+    let socket = ctx.socket(zmq::REQ).unwrap();
+    socket.connect("tcp://127.0.0.1:60265").unwrap();
+
+    for i in 0..3 {
+        let request = std::format!("Sending {i}th message");
+        socket.send(request.as_str(), 0).unwrap();
+
+        let msg_string = socket.recv_string(0).unwrap().unwrap();
+        println!("Received {msg_string}");
+    }
+
+    println!("Player: offline");
 }
--- world_server/src/main.rs
+++ world_server/src/main.rs
@@ -1,3 +1,24 @@
+extern crate zmq;
+
+// let 60265 be the SOL port
+
 fn main() {
-    println!("Hello, world!");
+    println!("World server: online");
+    let ctx = zmq::Context::new();
+
+    let socket = ctx.socket(zmq::REP).unwrap();
+    socket.bind("tcp://127.0.0.1:60265").unwrap();
+
+    let mut i = 0;
+
+    while i < 5 {
+        let msg_string = socket.recv_string(0).unwrap().unwrap();
+        println!("Received {msg_string}");
+
+        let reply = std::format!("Received {i}th message");
+        i += 1;
+        socket.send(reply.as_str(), 0).unwrap();
+    }
+
+    println!("World server: offline");
 }
