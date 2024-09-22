411548f6452cb62d0decd8cf89eaf72f440e2fa2
Geert van Ieperen <ieperen3039@outlook.com>
2024-09-22 14:24:16 +0200

    little bit of protocol work

--- player/src/minecraft_connection/minecraft_socket.rs
+++ player/src/minecraft_connection/minecraft_socket.rs
@@ -1,27 +1,57 @@
 use std::{
+    io,
     net::{SocketAddr, TcpListener, TcpStream},
     time::Duration,
-    io
 };
 
-use super::login;
+use minecraft_protocol::packets as mc_packets;
+
+use super::login::{self, CommunicationError};
+use super::network;
 
 pub struct Connection {}
 
 impl Connection {
-    pub fn await_connect() -> io::Result<Connection> {
+    pub fn await_connect() -> Result<Connection, CommunicationError> {
         let listener = TcpListener::bind("127.0.0.1:25567").expect("Failed to listen");
 
         // Accept 1 incoming connections
         let (stream, addr) = listener.accept()?;
-        
-        login::handle_connection(stream, addr)?;
-        
+        stream.set_read_timeout(Some(Duration::from_millis(50)))?;
+
+        let mut buffer = Vec::new();
+        let handshake_packet: mc_packets::handshake::ServerboundPacket =
+            network::receive_packet(&mut stream, &mut buffer)?;
+
+        let mc_packets::handshake::ServerboundPacket::Hello {
+            protocol_version,
+            server_address,
+            server_port,
+            next_state,
+        } = handshake_packet
+        else {
+            unreachable!()
+        };
+
+        let player_info = match next_state {
+            mc_packets::ConnectionState::Login => {
+                login::login(&mut stream, addr)?
+            },
+            mc_packets::ConnectionState::Status => {
+                todo!("Handle ConnectionState::Status")
+            },
+            _ => {
+                return Err(CommunicationError::UnexpectedState {
+                    from: mc_packets::ConnectionState::HandShake,
+                    to: next_state
+                })
+            },
+        };
+
         let player_info = login::initialize_client(&mut stream, player_info, world)?;
         let uuid = player_info.uuid;
         let eid = world.spawn_player(stream, player_info);
 
-        return Ok(Connection{});
+        return Ok(Connection {});
     }
-
 }
--- voxel_lib/src/world.rs
+++ voxel_lib/src/world.rs
@@ -1,23 +1,45 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::mpsc::{Receiver, Sender}};
 
-use crate::{chunk::Chunk64, vector_alias::Coordinate64};
+use crate::{chunk::{Chunk16, Chunk64}, vector_alias::{Coordinate, Coordinate64, Position}};
 
+enum WorldCommand {
+
+}
+
+pub struct ChunkColumn
+{
+    pub chunk_x : i32, 
+    pub chunk_y : i32, 
+    pub chunk_sections : Vec<Chunk16>,
+}
 pub struct World {
-    pub chunks: HashMap<(i32, i32, i32), Box<Chunk64>>,
+    queue_input: Sender<WorldCommand>,
+    queue: Receiver<WorldCommand>,
+    chunks: HashMap<Coordinate64, Box<Chunk64>>,
 }
 
 impl World {
     pub fn new() -> World {
+        let (sender, receiver) = std::sync::mpsc::channel();
         return World {
+            queue_input: sender,
+            queue : receiver,
             chunks: HashMap::new(),
         };
     }
 
+    pub fn get_message_queue(&self) -> Sender<WorldCommand> {
+        self.queue_input.clone()
+    }
+
     pub fn get_chunk<'s>(&'s self, coord : Coordinate64) -> Option<&'s Chunk64> {
-        self.chunks.get(&(coord.x, coord.y, coord.z)).map(|b| b.as_ref())
+        self.chunks.get(&coord).map(Box::as_ref)
     }
     
-    pub fn get_area(&self, player_position: nalgebra::OPoint<f32, nalgebra::Const<3>>) -> (HashMap<String, minecraft_protocol::nbt::NbtTag>, ChunkColumn) {
-        todo!()
+    pub fn get_area(&self, player_position: Position) -> (HashMap<String, minecraft_protocol::nbt::NbtTag>, Vec<ChunkColumn>) {
+        let heightmaps = HashMap::new();
+        let chunk_column = Vec::new();
+
+        return (heightmaps, chunk_column);
     }
 }
