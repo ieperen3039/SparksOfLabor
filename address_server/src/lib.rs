29e72597dc7ab67d3743b92c6f125f2c6dfece4e
Geert van Ieperen <ieperen3039@outlook.com>
2025-04-06 21:10:24 +0200

    cleanup

--- address_server/src/lib.rs
+++ address_server/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod static_addresses {
-    pub const LOG_SERVER : &str = "tcp://127.0.0.1:60266";
-    pub const WORLD_SERVER : &str = "tcp://127.0.0.1:60267";
+    pub const LOG_SERVER : &str = "tcp://localhost:60266";
+    pub const WORLD_SERVER : &str = "tcp://localhost:60267";
+    pub const MINECRAFT_SERVER_BIND : &str = "tcp://localhost:25565";
 }
